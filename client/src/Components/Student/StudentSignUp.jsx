d82a48f8063e53ac0501c8a09004ad02e3171a60
Keshav <keshav04ng@gmail.com>
2024-02-18 20:43:39 +0530

    Add signUp pages

--- client/src/Components/Student/StudentSignUp.jsx
+++ client/src/Components/Student/StudentSignUp.jsx
@@ -1,12 +1,66 @@
-import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Input,
+  Checkbox,
+  Button,
+} from "@material-tailwind/react";
 
-const StudentSignUp=()=>
-{
-    return(
-        <div>
-            Student signup Page
-        </div>
-    )
+import { useNavigate } from "react-router-dom";
+
+export default function InstitutionSignUp() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="h-screen flex items-center place-content-center">
+      <Card className="w-96">
+        <CardHeader
+          variant="gradient"
+          color="gray"
+          className=" fkex-1 mb-4 grid h-28 place-items-center"
+        >
+          <Typography variant="h3" color="white">
+            Student Sign Up
+          </Typography>
+        </CardHeader>
+        <CardBody className="flex flex-col gap-4">
+          <Input label="Name" type="lg" />
+          <Input label="Email" size="lg" />
+          <Input label="Password" size="lg" />
+          <div className="-ml-2.5">
+            <Checkbox label="Remember Me" />
+          </div>
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Button variant="gradient" fullWidth>
+            Sign Up
+          </Button>
+          <Typography variant="small" className="mt-6 flex justify-center">
+            Already have an account?
+            <Typography
+              as="a"
+              onClick={() => navigate("/student/login")}
+              variant="small"
+              color="blue-gray"
+              className="ml-1 font-bold cursor-pointer"
+            >
+              Sign In
+            </Typography>
+            <Typography
+              as="a"
+              onClick={() => navigate("/institution/signup")}
+              variant="small"
+              color="blue"
+              className="ml-1 font-bold cursor-pointer"
+            >
+              Institute?
+            </Typography>
+          </Typography>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
-
-export default StudentSignUp
