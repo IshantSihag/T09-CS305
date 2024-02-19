b258aef1d53eb546e99bdd3cc485178f9fcd12ab
jayrajsinh255 <102850190+jayrajsinh255@users.noreply.github.com>
2024-02-19 17:53:22 +0530

    Update InstitutionLoginPage.jsx

--- client/src/Components/Institution/InstitutionLoginPage.jsx
+++ client/src/Components/Institution/InstitutionLoginPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const InstitutionLogin=()=>
 {
     return(
-        <div className="bg-red-100">
+        <div >
             Institution Login Page
         </div>
     )
