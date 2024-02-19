2ff8d95b268b37e2351fc680e2ec0afa08ff1bd4
jayrajsinh255 <102850190+jayrajsinh255@users.noreply.github.com>
2024-02-19 18:41:19 +0530

    styling the profile page

--- client/src/Components/Institution/viewProfile.jsx
+++ client/src/Components/Institution/viewProfile.jsx
@@ -5,29 +5,28 @@ const ViewProfile = ({
     stored,
     startEditCallback
 }) => {
+    const unsplashImage='https://plus.unsplash.com/premium_photo-1707728599692-5b58748965d0?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
     return (
 
         <>
             <div className="min-h-screen bg-cover bg-center flex flex-col justify-center items-center">
-                <div className="mb-4 w-5/6">
-
-
-                    <Avatar
-                        src={stored.avatarImageUrl}
-                        alt="Avatar"
-                        size="xxl"
-                        className="border-4 border-white"
-                    />
-
-
+                <div className="mb-4 h-40 w-11/12 relative bg-cover bg-center rounded-2xl" style={{ backgroundImage: `url('${unsplashImage}')` }}>
+                    <div className="absolute inset-0 flex items-center justify-center">
+                        <Avatar
+                            src={stored.avatarImageUrl}
+                            alt="Avatar"
+                            size="xxl"
+                            className="border-4 border-white"
+                        />
+                    </div>
                 </div>
 
                 <div>
-                    <Card className="bg-white shadow-lg rounded-md p-6 m-auto">
+                    <Card className="bg-white shadow-xl rounded-md  p-6 m-auto w-11/12">
                         <CardBody>
                             <div className="mb-4">
                                 <div className="flex justify-center items-center mb-4">
-                                    {/* You can add additional content or styling here */}
+
                                 </div>
                                 <h5 className="text-gray-800 text-2xl font-bold mb-4">Institution Details</h5>
                                 <div className="grid grid-cols-3 gap-4">
@@ -54,17 +53,17 @@ const ViewProfile = ({
                             </div>
 
                             <div className="border-t border-gray-300 mt-4 pt-4">
-                                <h6 className="text-gray-700 text-lg font-semibold mb-2">About</h6>
-                                <p className="text-gray-600">
+                                <h6 className="text-gray-700 text-lg font-semibold">About</h6>
+                                <p className="text-gray-600 mb-10">
                                     {stored.about}
                                 </p>
                             </div>
-                            <div className=' m-2 absolute bottom-2 right-2' >
-                            <button onClick={startEditCallback}>
-                                <IconButton variant="filled" onclick={startEditCallback} >
-                                    <Pencil className='h-4 w-4' />
-                                </IconButton>
-                            </button>   
+                            <div className=' mb-5 mr-5 absolute bottom-0 right-0' >
+                                <button onClick={startEditCallback}>
+                                    <IconButton variant="filled" onclick={startEditCallback} >
+                                        <Pencil className='h-4 w-4' />
+                                    </IconButton>
+                                </button>
                             </div>
                         </CardBody>
                     </Card>
