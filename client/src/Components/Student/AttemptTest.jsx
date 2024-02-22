85599514d7deeaa9b6879566d946e5cc6e74d304
harshit kumar ravi <hkravi2002@gmail.com>
2024-02-22 14:48:40 +0530

    Add basic layout of the AttemptTest page and added its css file

--- client/src/Components/Student/AttemptTest.jsx
+++ client/src/Components/Student/AttemptTest.jsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { 
+    Button
+ } from "@material-tailwind/react";
+
+ //css 
+import "../../styles/AttemptTest.css";
 
 const AttemptTest=()=>
 {
     return (
         <div>
-            Attempt Test
+            <div className="test-container">
+                <div className="test-header">
+                    <div>ProctorX Test</div>
+                    <div>Pagination</div>
+                    <div className="test-header-timer">Time Left<span>00:00:00</span></div>
+                </div>   
+                <div className="test-body">
+                    <div className="question-container">
+                        <div className="question-meta-data">
+                            <div className="question-number">Question No. 1</div>
+                            <div className="question-points">Points: 10</div>
+                        </div>
+                        <div className="question-statement">Question Statement</div>
+                    </div>
+                    <div className="answer-container">
+                        <div className="answer-statement">Answer Statement</div>
+                        <div className="answer-option">Answer Options</div>
+                    </div>
+                </div>
+                <div className="test-footer">
+                    <Button className="test-submit-btn">Submit</Button>
+                    <div className="test-nav-btns">
+                        <Button className="test-prev-btn">Previous</Button>
+                        <Button className="test-next-btn">Next</Button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
