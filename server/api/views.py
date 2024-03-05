a6f88037c1728c9b1326583365aa0d613a6c9c0a
Anshul Mittal <mittalanshul091@gmail.com>
2024-03-05 13:35:02 +0530

    Removed migrations files and removed some mergin errors

--- server/api/views.py
+++ server/api/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import SignUpViewSerializer, MyTokenObtainPairSerializer, UserProfileSerializer
+from .serializers import SignUpViewSerializer, MyTokenObtainPairSerializer,ProfileViewSerializer, UserProfileSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .models import Test, UserProfile
@@ -72,11 +72,6 @@ class LogoutView(APIView):
                return Response(status=status.HTTP_400_BAD_REQUEST)
            
 class SignUpView(APIView):
-    def post(self, request):
-        serializer = SignUpViewSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
     def post(self, request):
         serializer = SignUpViewSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
