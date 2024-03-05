be94087f35f9f8d9be78ba7bc19fc87ba0d7ff49
jayrajsinh255 <102850190+jayrajsinh255@users.noreply.github.com>
2024-03-05 18:36:31 +0530

    GetAllTestsStudent api
    I have Implemented the get-all tests for the logged-in student.
    Small changes in Logoutview

--- server/api/urls.py
+++ server/api/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('startTest/', views.startTest.as_view(), name='startTest'),
     path('home/profile',views.ProfileView.as_view(),name='profile'),
     path('deleteTest/', views.deleteTest.as_view(), name='deleteTest'),
+    path('getAllTestsStudent/',views.GetAllTestStudentView.as_view(),name="getAllTestsStudent"),
 ]
