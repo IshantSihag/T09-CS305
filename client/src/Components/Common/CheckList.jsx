37e742313049a037a2a4725bdac433661bc29f7e
harshit kumar ravi <hkravi2002@gmail.com>
2024-03-10 16:39:43 +0530

    Integrate Attempt Test Page with backend

--- client/src/Components/Common/CheckList.jsx
+++ client/src/Components/Common/CheckList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { 
     Checkbox, Typography
 } from "@material-tailwind/react";
@@ -6,66 +6,26 @@ import {
 //css 
 import "../../styles/AttemptTest.css";
 
-const CheckList = ({}) => {
+const CheckList = ({
+    userQuestions, currentQuestion, handleOptionClick
+}) => {
     return (
         <div className="checklist">
-            <div className="checklist-item">
-                <Checkbox 
-                    id="checkbox-1"
-                    ripple={true}
-                    className="hover:before:opacity-0"
-                    containerProps={{
-                    className: "p-0",
-                    }}
-                />
-                <Typography color="blue-gray" className="font-medium">
-                    React.js
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </Typography>
-            </div>
-            <div className="checklist-item">
-                <Checkbox 
-                    id="checkbox-2"
-                    ripple={false}
-                    className="hover:before:opacity-0"
-                    containerProps={{
-                    className: "p-0",
-                    }}
-                />
-                <Typography color="blue-gray" className="font-medium">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                </Typography>
-            </div>
-            <div className="checklist-item">
-                <Checkbox 
-                    id="checkbox-3"
-                    ripple={false}
-                    className="hover:before:opacity-0"
-                    containerProps={{
-                    className: "p-0",
-                    }}
-                />
-                <Typography color="blue-gray" className="font-medium">
-                    Duis aute irure dolor in reprehenderit in 
-                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                </Typography>
-            </div>
-            <div className="checklist-item">
-                <Checkbox 
-                    id="checkbox-4"
-                    ripple={false}
-                    className="hover:before:opacity-0"
-                    containerProps={{
-                    className: "p-0",
-                    }}
-                />
-                <Typography color="blue-gray" className="font-medium">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </Typography>
-            </div>
+            {userQuestions.length !== 0 && userQuestions[currentQuestion-1].options[0].map((opt, index) => (
+                <div className="checklist-item" key={index} onClick={(e) => handleOptionClick(e, index)}>
+                    <Checkbox 
+                        id="checkbox-1"
+                        ripple={true}
+                        className="hover:before:opacity-0"
+                        containerProps={{
+                            className: "p-0",
+                        }}
+                        // onClick={(e) => handleOptionClick(e, index)}
+                        checked={userQuestions[currentQuestion-1].answerList.includes(index)}
+                    />
+                    <Typography color="blue-gray" className="font-medium">{opt}</Typography>
+                </div> 
+            ))}
         </div>
     )
 }
