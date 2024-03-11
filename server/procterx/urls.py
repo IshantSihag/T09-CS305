bd1dd038eae44d00e98d7c0670b43d479ad26783
jayrajsinh255 <102850190+jayrajsinh255@users.noreply.github.com>
2024-03-11 17:03:06 +0530

    added an app to avoid conflicts

--- server/procterx/urls.py
+++ server/procterx/urls.py
@@ -22,8 +22,7 @@ urlpatterns = [
     path('token/', 
           jwt_views.TokenObtainPairView.as_view(), 
           name ='token_obtain_pair'),
-     path('token/refresh/', 
-          jwt_views.TokenRefreshView.as_view(), 
-          name ='token_refresh'),
-     path('', include('api.urls')),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh'),
+    path('', include('api.urls')),
+    path('', include('resultsapi.urls')),
 ]
