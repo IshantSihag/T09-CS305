6256aa92458f41d482876e79a824a2d7def2e4bf
jayrajsinh255 <102850190+jayrajsinh255@users.noreply.github.com>
2024-03-11 22:21:14 +0530

    Update views.py

--- server/resultsapi/views.py
+++ server/resultsapi/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
-from api.models import Test, UserProfile
+from api.models import Test, UserProfile,Question,Response as ResponseModel
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 
