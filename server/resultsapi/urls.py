87cfd8a9d6d8b1f0a8da4bc3038bf63b8ce08c94
jayrajsinh255 <102850190+jayrajsinh255@users.noreply.github.com>
2024-03-11 21:27:29 +0530

    Update urls.py

--- server/resultsapi/urls.py
+++ server/resultsapi/urls.py
@@ -4,4 +4,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('student/registerForTest', views.RegisterStudentForTestView.as_view(), name='RegisterStudentForTest'),
+    path('student/getResultForTest',views.GetResultForStudent.as_view(),name='GetResultForStudent'),
 ]
