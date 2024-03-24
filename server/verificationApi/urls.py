76642618b28498f672799f0a7f0b36953be886e0
Anant Prakash Singh <anant733n@gmail.com>
2024-03-24 13:11:28 +0530

    added submitInitailImage endpoint to urls.py

--- server/verificationApi/urls.py
+++ server/verificationApi/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("verifyUser/", views.VerifyUserView.as_view(), name="verifyUser"),
+    path("submitInitailImage/", views.SubmitInitalImage.as_view(), name="submitInitailImage"),
 ]
