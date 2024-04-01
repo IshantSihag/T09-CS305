1c004b89d4d02148f32b74e04598f1602a0f8de4
Anshul Mittal <mittalanshul091@gmail.com>
2024-04-01 18:38:07 +0530

    Logout corrected

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenBlacklistView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("home/", views.HomeView.as_view(), name="home"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("startTest/", views.startTest.as_view(), name="startTest"),
