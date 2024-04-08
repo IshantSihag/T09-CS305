5887f9c7c54bfa17924e53e33f4e9e3bed47e0a5
Anjali <anjaliofficail1@gmail.com>
2024-04-08 23:31:34 +0530

    formatted the code

--- server/testsapi/tests/test_result_institution.py
+++ server/testsapi/tests/test_result_institution.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from django.urls import reverse
 import json
 
+
 class TestResultInstitute(TestCase):
     def setUp(self):
         # setting up and creation of a test student
@@ -98,9 +99,11 @@ class TestResultInstitute(TestCase):
         self.assertTrue(self.response.data["ok"])
         self.assertTrue(self.response.data["message"])
         self.assertTrue(self.response.data["score"])
+
     def test_result_institute(self):
         self.result_url = reverse("testresult")
-        headers = {"HTTP_AUTHORIZATION": "Bearer " + self.jwt_token,
+        headers = {
+            "HTTP_AUTHORIZATION": "Bearer " + self.jwt_token,
         }
         self.result_url += f"?test_id={self.test_id}"
         self.response = self.client.get(self.result_url, format=json, **headers)
@@ -111,16 +114,16 @@ class TestResultInstitute(TestCase):
         self.assertTrue(self.response.data["result"][0]["score"])
         self.assertTrue(self.response.data["result"][0]["name"])
 
-
     def test_result_no_test_id(self):
         self.result_url = reverse("testresult")
-        headers = {"HTTP_AUTHORIZATION": "Bearer " + self.jwt_token,
+        headers = {
+            "HTTP_AUTHORIZATION": "Bearer " + self.jwt_token,
         }
         self.response = self.client.get(self.result_url, format=json, **headers)
         # print(self.response.data)
         self.assertEqual(self.response.status_code, 400)
         self.assertFalse(self.response.data["ok"])
-    
+
     def test_result_no_auth(self):
         self.result_url = reverse("testresult")
         self.response = self.client.get(self.result_url)
@@ -151,20 +154,21 @@ class TestResultInstitute(TestCase):
         self.assertTrue(self.response.data["ok"])
         self.jwt_token = self.response.data["access"]
         self.result_url = reverse("testresult")
-        headers = {"HTTP_AUTHORIZATION": "Bearer " + self.jwt_token,
+        headers = {
+            "HTTP_AUTHORIZATION": "Bearer " + self.jwt_token,
         }
         self.result_url += f"?test_id={self.test_id}"
         self.response = self.client.get(self.result_url, format=json, **headers)
         self.assertEqual(self.response.status_code, 401)
         self.assertFalse(self.response.data["ok"])
         self.assertTrue(self.response.data["error"])
-    
+
     def pending_test_result_invalid_test_id(self):
         self.result_url = reverse("testresult")
-        headers = {"HTTP_AUTHORIZATION": "Bearer " + self.jwt_token,
+        headers = {
+            "HTTP_AUTHORIZATION": "Bearer " + self.jwt_token,
         }
         self.result_url += f"?test_id=0"
         self.response = self.client.get(self.result_url, format=json, **headers)
         self.assertEqual(self.response.status_code, 400)
-        self.assertFalse(self.response.data["ok"])    
-
+        self.assertFalse(self.response.data["ok"])
