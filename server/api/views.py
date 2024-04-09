d7c97de3aa144fa95bdba5c433794bf16c52c1bf
jayrajsinh255 <102850190+jayrajsinh255@users.noreply.github.com>
2024-04-09 18:30:04 +0530

    formatting

--- server/api/views.py
+++ server/api/views.py
@@ -489,8 +489,8 @@ class FetchStudentDetails(APIView):
                 "cgpa": student.cgpa,
                 "batch": student.batch,
                 "course": student.course,
-                "bio":userprofile.bio,
-                "profile_url":userprofile.profile_url
+                "bio": userprofile.bio,
+                "profile_url": userprofile.profile_url,
             }
             return Response(jsonresponse, status=status.HTTP_200_OK)
 
