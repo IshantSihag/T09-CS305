b624fdad3940d1c4048e540b1293ef9b23104977
Anjali <anjaliofficail1@gmail.com>
2024-04-13 23:51:57 +0530

    updated submit test cases

--- server/testsapi/tests/test_submit_test.py
+++ server/testsapi/tests/test_submit_test.py
@@ -1,13 +1,18 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from datetime import datetime
 import json
-
 
 class TestCreateTest(TestCase):
     def setUp(self):
-        # setting up and creation of a test student
+
+        # Basic urls and client set up
         self.client = Client()
         self.signup_url = reverse("signup")
+        self.login_url = reverse("login")
+        self.register_url = "/student/registerForTest/"
+
+        # signing up a student
         signup_data = {
             "username": "teststudent@example.com",
             "password": "testpassword",
@@ -18,9 +23,8 @@ class TestCreateTest(TestCase):
         self.response = self.client.post(self.signup_url, signup_data)
         self.assertEqual(self.response.status_code, 201)
 
-        # setting up and creation of a test institute
+        # signing up an institute
         self.client = Client()
-        self.signup_url = reverse("signup")
         signup_data = {
             "username": "testinstitute@example.com",
             "password": "testpassword",
@@ -31,8 +35,7 @@ class TestCreateTest(TestCase):
         self.response = self.client.post(self.signup_url, signup_data)
         self.assertEqual(self.response.status_code, 201)
 
-        # logging in
-        self.login_url = reverse("login")
+        # logging in institute for creation of test
         login_data = {
             "username": "testinstitute@example.com",
             "password": "testpassword",
@@ -41,10 +44,12 @@ class TestCreateTest(TestCase):
         self.response = self.client.post(self.login_url, login_data)
         self.assertEqual(self.response.status_code, 200)
         self.assertTrue(self.response.data["ok"])
-        self.jwt_token = self.response.data["access"]
+        self.jwt_token_institute = self.response.data["access"]
+
+        # creating a test
         create_test_data = {
             "title": "QUIZ",
-            "start": "2024-03-24 21:11 +0000",
+            "start": f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} +0000',
             "duration": "3600",
             "questions": json.dumps(
                 [
@@ -69,15 +74,36 @@ class TestCreateTest(TestCase):
                 ]
             ),
         }
-        headers = {"HTTP_AUTHORIZATION": "Bearer " + self.jwt_token}
+        headers = {"HTTP_AUTHORIZATION": "Bearer " + self.jwt_token_institute}
         self.response = self.client.post(
             reverse("createTest"), create_test_data, **headers
         )
-        self.test_id = self.response.data["test_id"]
         self.assertEqual(self.response.status_code, 201)
         self.assertTrue(self.response.data["ok"])
         self.assertTrue(self.response.data["test_id"])
+        self.test_id = self.response.data["test_id"]
         self.assertTrue(self.response.data["testCode"])
+
+
+        # logging in student for registration of test
+        login_data = {
+            "username": "teststudent@example.com",
+            "password": "testpassword",
+            "type": "student",
+        }
+        self.response = self.client.post(self.login_url, login_data)
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTrue(self.response.data["ok"])
+        self.jwt_token = self.response.data["access"]
+
+        # student registring for the test
+        register_data = {
+            "test_id": self.test_id,
+        }
+        headers = {"HTTP_AUTHORIZATION": "Bearer " + self.jwt_token}
+        self.response = self.client.post(self.register_url, register_data, **headers)
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTrue(self.response.data["ok"])
 
     def test_submit_test(self):
         submit_test_data = {
@@ -97,6 +123,7 @@ class TestCreateTest(TestCase):
         self.response = self.client.post(
             reverse("submitTest"), submit_test_data, **headers
         )
+        # print(self.response.data)
         self.assertEqual(self.response.status_code, 200)
         self.assertTrue(self.response.data["ok"])
         self.assertTrue(self.response.data["message"])
@@ -146,27 +173,25 @@ class TestCreateTest(TestCase):
         self.assertFalse(self.response.data["ok"])
         self.assertTrue(self.response.data["error"])
 
-    def test_submit_test_invalid_question_id(self):
+    def test_submit_test_no_auth(self):
         submit_test_data = {
             "data": json.dumps(
                 {
                     "test_id": str(self.test_id),
                     "user_response": [
                         {
-                            "id": 2,
+                            "id": 1,
                             "answerList": [0],
                         }
                     ],
                 }
             ),
         }
-        headers = {"HTTP_AUTHORIZATION": "Bearer " + self.jwt_token}
+        headers = {}
         self.response = self.client.post(
             reverse("submitTest"), submit_test_data, **headers
         )
-        self.assertEqual(self.response.status_code, 400)
-        self.assertFalse(self.response.data["ok"])
-        self.assertTrue(self.response.data["error"])
+        self.assertEqual(self.response.status_code, 401)
 
     def test_submit_test_invalid_question_not_part_of_test(self):
         submit_test_data = {
